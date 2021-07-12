3442f4e6e9c2c4a1109992e64b6380ba500723a0
Christian Schuhmann <christian8246q@gmail.com>
2021-07-12 20:17:15 +0200

    Derp fix

--- TestClass.java
+++ TestClass.java
@@ -1,35 +1,31 @@
 class TestClass{
     int test;
-    String i;
+    char c;
 
-    void test(int a, String b) {
-        System.out.println(a + b);
-    }
+    void test2(int a, String b) {
 
-    int test2(int a, String b) {
-        return a;
     }
 
     String t;
 
     int test32(int a) {
         if (true || false) {
-            return 5;
-        } else
-            System.out.println("Else");
-        t = t;
-        int i;
-        t = null;
-        t = "5";
-        i = 3;
-        Object o = new Object();
-        super.toString();
-        this.test2(5, "Hallo");
-        while (i > 0) {
-            System.out.println("Schleifendurchgang");
-            i = 0;
+            return 3;
         }
+        t = t;
+        Object o;
+        t = null;
+        t = "5head";
+        c = 'X';
+        o = new Object();
+        silas();
+        while (a == 5) {
+            a = 1337;
+        }
+        return a;
+    }
 
-        return 69;
+    String silas() {
+        return "BIER";
     }
 }
